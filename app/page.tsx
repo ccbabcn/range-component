4385adf878c35e3e1f64d018dd57c725412dec83
Cristian <cristian@Cristians-MacBook-Pro.local>
2024-04-13 21:34:10 +0200

    Adds initial test configuration and dummy app page test

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+const unused = 0;
+
 export default function Page() {
-  return <h1 className="text-blue-500 font-bold text-xl">Hello, Next.js!</h1>;
+  return (
+    <div>
+      <h1 className="text-blue-500 font-bold text-xl">Hello, Next.js!</h1>
+    </div>
+  );
 }
