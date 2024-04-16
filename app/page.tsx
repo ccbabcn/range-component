e0c96e6a400022f1830ff054ef28512e7edb6d4a
Cristian <cristian@Cristians-MacBook-Pro.local>
2024-04-17 01:33:26 +0200

    Updates layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Range Component',
+  description: 'Next.js Range Component',
+  keywords: 'range component, next.js, typescript',
+  authors: [
+    { name: 'Cristian Bermudez Agudelo', url: 'https://github.com/ccbabcn' },
+  ],
+  creator: 'Cristian Bermudez Agudelo',
+  icons: '/favicon.ico',
+};
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-xl font-bold text-blue-500">Hello, Next.js!</h1>
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-y-4">
+      <h1 className="text-xl font-bold text-blue-500">Range component</h1>
+      <div className="w-1/2">Range here</div>
     </div>
   );
 }
