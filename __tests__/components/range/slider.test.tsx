2c4bf5e25e13884b4f269a20905055f33b168c49
Cristian <cristian@Cristians-MacBook-Pro.local>
2024-04-19 04:59:55 +0200

    updates Slider component and its test

--- __tests__/components/range/slider.test.tsx
+++ __tests__/components/range/slider.test.tsx
@@ -4,7 +4,7 @@ import { render, fireEvent } from '@testing-library/react';
 describe('GIVEN a Slider component ', () => {
   describe('WHEN instantiated', () => {
     it('THEN it should render two knobs and a progress bar', () => {
-      const { getByTestId } = render(<Slider />);
+      const { getByTestId } = render(<Slider onChange={() => {}} />);
 
       const leftKnob = getByTestId('left-knob');
       const rigthKnob = getByTestId('right-knob');
@@ -18,13 +18,15 @@ describe('GIVEN a Slider component ', () => {
 
   describe('WHEN it is rendered and the user interacts with a knob', () => {
     it('THEN should update knob position accordingly', () => {
-      const { getByTestId } = render(<Slider />);
+      const mockOnChange = jest.fn();
+      const { getByTestId } = render(<Slider onChange={mockOnChange} />);
       const leftKnob = getByTestId('left-knob');
 
       fireEvent.mouseDown(leftKnob);
       fireEvent.mouseMove(document, { clientX: 15 });
 
       expect(leftKnob.style.left).toBe('15px');
+      expect(mockOnChange).toHaveBeenCalled();
     });
   });
 });
